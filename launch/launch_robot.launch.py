a8f6a75fdd9186e2c23c29cdab6b4fcecae0991e
willgpaik <bgh0919@gmail.com>
2024-07-07 17:29:31 -0400

    joystick

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -28,6 +28,13 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
+    # joystick = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory(package_name),'launch','joystick.launch.py'
+    #             )])
+    # )
+
+
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
     controller_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'my_controllers.yaml')
 
@@ -87,6 +94,7 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
+        # joystick,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner
